8ffdeacbdc0195f809dea142a382f3ea8e844780
Kevin Holligan <kholligan@gmail.com>
2020-01-06 15:36:36 -0700

    SSDESKTOP-2452 : extracting script loop and version history file

---
+++ docker/electron_versions_runner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p symbols && cd symbols
+while IFS=" " read version directory; do
+  curl -L -o darwin.x64.zip "https://github.com/electron/electron/releases/download/v$version/electron-v$version-darwin-x64-symbols.zip" && unzip darwin.x64.zip && mv -f "$directory"/* ../../pool/symbols && rm -rf ./*
+  curl -L -o win32.zip "https://github.com/electron/electron/releases/download/v$version/electron-v$version-win32-ia32-symbols.zip" && unzip win32.zip && mv -f "$directory"/* ../../pool/symbols && rm -rf ./*
+done < ../electron_versions.txt
+
