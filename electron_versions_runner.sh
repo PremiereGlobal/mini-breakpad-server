58537370cf3f57282a63a50a64869a6987990531
Kevin Holligan <kholligan@gmail.com>
2020-01-16 09:44:10 -0700

    SSDESKTOP-2477 : update version_runner to also extract x64 symbols for x64 releases

--- electron_versions_runner.sh
+++ electron_versions_runner.sh
@@ -2,6 +2,7 @@
 mkdir -p symbols && cd symbols
 while IFS=" " read version directory; do
   curl -L -o darwin.x64.zip "https://github.com/electron/electron/releases/download/v$version/electron-v$version-darwin-x64-symbols.zip" && unzip darwin.x64.zip && rsync -a "$directory"/ /app/pool/symbols && rm -rf ./*
-  curl -L -o win32.zip "https://github.com/electron/electron/releases/download/v$version/electron-v$version-win32-ia32-symbols.zip" && unzip win32.zip && rsync -a "$directory"/ /app/pool/symbols && rm -rf ./*
+  curl -L -o win32.ia32.zip "https://github.com/electron/electron/releases/download/v$version/electron-v$version-win32-ia32-symbols.zip" && unzip win32.ia32.zip && rsync -a "$directory"/ /app/pool/symbols && rm -rf ./*
+  curl -L -o win32.x64.zip "https://github.com/electron/electron/releases/download/v$version/electron-v$version-win32-x64-symbols.zip" && unzip win32.x64.zip && rsync -a "$directory"/ /app/pool/symbols && rm -rf ./*
 done < ../electron_versions.txt
 
